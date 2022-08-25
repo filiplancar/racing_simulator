3d422e89942ef6cd4272e8284f413156ff2f7813
Filip Lancaric <filiplancaric222@gmail.com>
2022-08-25 15:07:20 +0200

    Save images

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import pygame
+import os
 
 pygame.init()
+
+BG = pygame.image.load(os.path.join('assets', 'background-1.png'))
+CAR_0 = pygame.image.load(os.path.join('assets', 'car0.png'))
+CAR_1 = pygame.image.load(os.path.join('assets', 'car1.png'))
