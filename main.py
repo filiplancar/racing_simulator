9694659a2fab767761706784a5511fecc68a9331
Filip Lancaric <filiplancaric222@gmail.com>
2022-08-25 15:27:34 +0200

    Save sizes to class

--- main.py
+++ main.py
@@ -6,15 +6,27 @@ import os
 pygame.init()
 
 #Sizes
-FONT_SIZE = 20
-WIDTH = 840
-HEIGHT = 650
+class Sizes:
+    def __init__(self, font_size, width, height):
+        self.font_size = font_size
+        self.width = width
+        self.height = height
+
+s = Sizes(20, 840, 650)
+
 
 #Images and font
 BG = pygame.image.load(os.path.join('assets', 'background-1.png'))
 CAR_0 = pygame.image.load(os.path.join('assets', 'car0.png'))
 CAR_1 = pygame.image.load(os.path.join('assets', 'car1.png'))
-FONT = pygame.font.Font('Arial', FONT_SIZE)
+FONT = pygame.font.Font('Arial', s.font_size)
 
-pygame.display.set_mode((WIDTH, HEIGHT))
+#Set display
+pygame.display.set_mode((s.width, s.height))
 pygame.display.set_caption('Racing Simulator')
+
+#Game loop
+# running = True
+
+# while running:
+#     for event
