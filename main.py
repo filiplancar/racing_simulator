80a5ddc0c542c514843fdb3d61cfff410a2cc494
Filip Lancaric <filiplancaric222@gmail.com>
2022-08-25 18:15:43 +0200

    Get background throw class

--- main.py
+++ main.py
@@ -14,8 +14,18 @@ class Sizes:
 
 s = Sizes(20, 840, 650)
 
+#Get background
+class Background(pygame.sprite.Sprite):
+    def __init__(self, image_file, location):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(image_file)
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
+
+bg = Background(os.path.join('assets', 'background-1.png', [0,0]))
+
 #Images and font
-BG = pygame.image.load(os.path.join('assets', 'background-1.png'))
+# BG = pygame.image.load(os.path.join('assets', 'background-1.png'))
 CAR_0 = pygame.image.load(os.path.join('assets', 'car0.png'))
 CAR_1 = pygame.image.load(os.path.join('assets', 'car1.png'))
 FONT = pygame.font.SysFont('Arial', s.font_size)
