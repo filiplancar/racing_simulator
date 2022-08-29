30e037b93aae67b22ca7d8e56a4c2b11aecd4419
Filip Lancaric <filiplancaric222@gmail.com>
2022-08-29 16:29:28 +0200

    fill and blit screen with bg

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 #Import modules
 import pygame
 import os
+from pygame.locals import *
 
 #Initialize pygame
 pygame.init()
+
+#Colors
+WHITE = (255,255,255)
 
 #Sizes
 class Sizes:
@@ -14,24 +18,23 @@ class Sizes:
 
 s = Sizes(20, 840, 650)
 
-#Get background
-class Background(pygame.sprite.Sprite):
-    def __init__(self, image_file, location):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(image_file)
-        self.rect = self.image.get_rect()
-        self.rect.left, self.rect.top = location
-
-bg = Background(os.path.join('assets', 'background-1.png', [0,0]))
-
 #Images and font
-# BG = pygame.image.load(os.path.join('assets', 'background-1.png'))
 CAR_0 = pygame.image.load(os.path.join('assets', 'car0.png'))
 CAR_1 = pygame.image.load(os.path.join('assets', 'car1.png'))
 FONT = pygame.font.SysFont('Arial', s.font_size)
 
+#Get background
+class Background(pygame.sprite.Sprite):
+    def __init__(self, path, image_file, location):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join(path, image_file))
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
+
+bg = Background('assets', 'background-1.png', (0,0))
+
 #Set display
-WIN = pygame.display.set_mode((s.width, s.height))
+screen = pygame.display.set_mode((s.width, s.height))
 pygame.display.set_caption('Racing Simulator')
 
 #Game loop
@@ -42,6 +45,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            
+        screen.fill(WHITE)    
+        screen.blit(bg.image, bg.rect)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
